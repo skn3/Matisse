1aa880d6ac99730201db4d651fb3d097ca8f2d7b
James Kong <james.kong@nixplay.com>
2017-12-01 14:17:51 +0800

    Fixed some issue

--- matisse/src/main/java/com/zhihu/matisse/internal/ui/PreviewItemFragment.java
+++ matisse/src/main/java/com/zhihu/matisse/internal/ui/PreviewItemFragment.java
@@ -27,11 +27,13 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 import com.zhihu.matisse.R;
 import com.zhihu.matisse.internal.entity.Item;
 import com.zhihu.matisse.internal.entity.SelectionSpec;
 import com.zhihu.matisse.internal.utils.PhotoMetadataUtils;
+
+import it.sephiroth.android.library.imagezoom.ImageViewTouch;
+import it.sephiroth.android.library.imagezoom.ImageViewTouchBase;
 
 public class PreviewItemFragment extends Fragment {
 
@@ -77,23 +79,21 @@ public class PreviewItemFragment extends Fragment {
             videoPlayButton.setVisibility(View.GONE);
         }
 
-        SubsamplingScaleImageView scaleImageView = (SubsamplingScaleImageView) view.findViewById(R.id.image_view_sub);
-        scaleImageView.setOrientation(SubsamplingScaleImageView.ORIENTATION_USE_EXIF);// 自动旋转
-        scaleImageView.setDoubleTapZoomDuration(200);// 放大动画时长
-        scaleImageView.setDoubleTapZoomStyle(SubsamplingScaleImageView.ZOOM_FOCUS_CENTER);
+        ImageViewTouch image = (ImageViewTouch) view.findViewById(R.id.image_view);
+        image.setDisplayType(ImageViewTouchBase.DisplayType.FIT_TO_SCREEN);
 
         ImageView imageViewGif = (ImageView) view.findViewById(R.id.image_view_gif);
 
         Point size = PhotoMetadataUtils.getBitmapSize(item.getContentUri(), getActivity());
         if (item.isGif()) {
-            scaleImageView.setVisibility(View.GONE);
+            image.setVisibility(View.GONE);
             imageViewGif.setVisibility(View.VISIBLE);
             SelectionSpec.getInstance().imageEngine.loadGifImage(getContext(), size.x, size.y, imageViewGif,
                     item.getContentUri());
         } else {
             imageViewGif.setVisibility(View.GONE);
-            scaleImageView.setVisibility(View.VISIBLE);
-            SelectionSpec.getInstance().imageEngine.loadImage(getContext(), size.x, size.y, scaleImageView,
+            image.setVisibility(View.VISIBLE);
+            SelectionSpec.getInstance().imageEngine.loadImage(getContext(), size.x, size.y, image,
                     item.getContentUri());
         }
     }
